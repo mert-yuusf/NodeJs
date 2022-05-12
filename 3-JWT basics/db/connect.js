b5013bc6c85f8c754e372ea7af44d5aca7658e5a
mert-yuusf <mertizgahi900@gmail.com>
2022-05-12 15:32:57 +0300

    update

--- 3-JWT basics/db/connect.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDb = async (url) => {
-    await mongoose.connect(url)
-        .then(() => console.log('connected to database'))
-        .catch((error) => console.log(error));
-}
-
-module.exports = connectDb;
