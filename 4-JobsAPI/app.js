00d6bdf517f043f28881ad888df6760ed85b51a4
mert-yuusf <mertizgahi900@gmail.com>
2022-05-13 18:24:13 +0300

    finish jobs api

--- 4-JobsAPI/app.js
+++ 4-JobsAPI/app.js
@@ -4,10 +4,17 @@ require('dotenv').config();
 require('express-async-errors');
 
 // extra security packages
-const helmet = require()
-const cors = require()
-const xss = require()
-const ratelimit = require()
+const helmet = require("helmet")
+const cors = require("cors")
+const xss = require("xss-clean")
+const rateLimiter = require("express-rate-limit")
+
+// swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocs = YAML.load('./swagger.yaml');
+
+
 const app = express();
 // app config
 const PORT = process.env.PORT || 3000;
@@ -25,8 +32,24 @@ const jobRouter = require('./routes/jobs')
 // app.use
 app.use(express.json());
 app.use(errorHandlerMiddleware);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 100,
+    max: 100,
+}));
 
 // register routers
+app.get('/', (req, res) => {
+    res.send(`
+    <h1>Jobs API</h1>
+    <p>To see all documentation follow link</p>
+    <a href="/docs" >docs</a>
+    `)
+})
+app.use('/docs', swaggerUI.serve,
+    swaggerUI.setup(swaggerDocs))
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authorizerUser, jobRouter);
 
